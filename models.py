6d7d0ac88c7aefab222da6ec38c33a72db2e60ac
maksasGit <maksim.rishko.vit@gmail.com>
2026-01-22 01:01:53 +0100

    [Blind fix] model relation

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 # models.py
 
-
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
@@ -12,8 +11,14 @@ class Category(Base):
     descr = Column(String)
     parent_id = Column(Integer, ForeignKey("categories.id"), nullable=True)
     
-    children = relationship("Category", backref="parent", remote_side=[id])
+    parent = relationship(
+        "Category",
+        remote_side=[id],
+        backref="children",
+        uselist=False  # <--- один объект parent, не список
+    )
     entries = relationship("Entry", backref="category")
+
 
 class Entry(Base):
     __tablename__ = "entries"
